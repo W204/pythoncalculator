f7e7e92035ba96c067e33081addaedd18b23aad0
W204 <45856280+W204@users.noreply.github.com>
2018-12-13 17:32:35 +0000

    Add files via upload

---
+++ Calculator_ifElifElse.py
@@ -0,0 +1,28 @@
+number1_str = input(" First number: ")
+number1 = int(number1_str)
+
+number2_str = input("Second number: ")
+number2 = int(number2_str)
+
+operation = input("Operation [+, -, *, /]: ")
+
+if operation == "+":
+    combination = number1 + number2
+elif operation == "-":
+    combination = number1 - number2
+elif operation == "*":
+    combination = number1 * number2
+elif operation == "/":
+    if number2 == 0:
+        print("You cant divide by 0")
+        combination = 0
+    else:
+        combination = number1 / number2     
+else:
+    combination = "ERROR ... '" + operation + "' unrecognised"
+
+combination_str = str(combination)
+print("Answer: " + combination_str)
+
+print()
+input("Press return to continue ...")
